74c108a450974cb21226fe1d5aac430e80b4c327
Peter Crona <petercrona89@gmail.com>
2019-04-08 22:40:47 +0200

    clean and use ref for action

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import * as Snake from 'jsunconf-snake';
 import * as R from 'ramda';
 import { createTile } from './createTile';
 import { GameStatus } from './GameStatus';
 import { useInterval } from './useInterval';
-import { id, withIndex, getSeed } from './utils';
+import { withIndex, getSeed } from './utils';
 
 export interface Tile {
   pos: [number, number],
   className: string
 }
 
+const createGame = () => Snake.newGame({
+  seed: getSeed(),
+  width: 20,
+  height: 20
+});
+
 const App = () => {
   // == Init
-  const [game, setGame] = useState(Snake.newGame({
-    seed: getSeed(), 
-    width: 20, 
-    height: 20
-  }));
-  const [action, setAction] = useState({ fn: id });
+  const [game, setGame] = useState(createGame());
+  const action = useRef((x: any) => x);
 
   // == Controller (handle key press)
   const keyPressHandler = R.compose(R.cond([
-    [R.equals('ArrowUp'), () => setAction({ fn: Snake.moveUp })],
-    [R.equals('ArrowDown'), () => setAction({ fn: Snake.moveDown })],
-    [R.equals('ArrowLeft'), () => setAction({ fn: Snake.moveLeft })],
-    [R.equals('ArrowRight'), () => setAction({ fn: Snake.moveRight })]
+    [R.equals('ArrowUp'), () => action.current = Snake.moveUp],
+    [R.equals('ArrowDown'), () => action.current = Snake.moveDown],
+    [R.equals('ArrowLeft'), () => action.current = Snake.moveLeft],
+    [R.equals('ArrowRight'), () => action.current = Snake.moveRight]
   ]), R.prop('key'));
 
   useEffect(() => {
@@ -38,37 +40,37 @@ const App = () => {
 
   // Start game loop
   useInterval(() => {
-    setGame(Snake.tick(action.fn(game)));
+    setGame(Snake.tick(action.current(game)));
   }, 50);
 
   // == View
   const width = Snake.getWidth(game);
   const height = Snake.getHeight(game);
   const ratio = height / width;
-  const ratioHeight = 500;
+  const ratioHeight = 300;
   const ratioWidth = ratioHeight / ratio;
 
   const Tile = createTile(width, height);
 
   return (
     <>
-    <GameStatus game={game} />
+      <GameStatus game={game} />
 
-    <div className="container" style={{height: ratioHeight, width: ratioWidth}}>
+      <div className="container" style={{ height: ratioHeight, width: ratioWidth }}>
 
-      {R.map(([index, vector]) =>
-        <Tile key={index} className='wallTile' pos={vector} />,
-        withIndex(Snake.getWalls(game)))}
+        {R.map(([index, vector]) =>
+          <Tile key={index} className='wallTile' pos={vector} />,
+          withIndex(Snake.getWalls(game)))}
 
-      {R.map(([index, vector]) =>
-        <Tile key={index} className='snakeTile' pos={vector} />,
-        withIndex(Snake.getSnake(game)))}
+        {R.map(([index, vector]) =>
+          <Tile key={index} className='snakeTile' pos={vector} />,
+          withIndex(Snake.getSnake(game)))}
 
-      {R.map((vector) =>
-        <Tile key={R.toString(vector)} className='appleTile' pos={vector} />,
-        Snake.getApples(game))}
+        {R.map((vector) =>
+          <Tile key={R.toString(vector)} className='appleTile' pos={vector} />,
+          Snake.getApples(game))}
 
-    </div>
+      </div>
     </>
   );
 };
--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,4 @@ import zip from "ramda/es/zip";
 import range from "ramda/es/range";
 
 export const withIndex = (xs: any[]) => zip(range(0, xs.length), xs);
-export const id = (x: any) => x;
 export const getSeed = () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
